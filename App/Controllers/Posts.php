559e8a9ace2402a9e928e147bea421b502a9521d
predragbilbija <predrag.bilbija996@gmail.com>
2022-07-22 11:31:07 +0200

    autoloader_removeQueryString

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Controllers;
+
 /**
  * Posts controller
  *
@@ -15,6 +17,8 @@ class Posts
     public function index()
     {
         echo 'Hello from the index action in the Posts controller!';
+        echo '<p>Query string parameters: <pre>' .
+             htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
     }
 
     /**
